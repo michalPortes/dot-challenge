e4c1739f9154ba4679eb41ebea37c30b71d54107
Michael Portes <michael11portes@hotmail.com>
2022-12-30 01:31:19 -0300

    desafio de adicionar e remover pontos em uma tela

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
+import './App.css'
+
+const App = () => {
+  const [list, setList] = useState([])
+  const [listRedo, setListRedo] = useState([])
+
+  const Dot = event => {
+    const newDot = {
+      clientX: event.clientX,
+      clientY: event.clientY
+    }
+    setList(prev => [...prev, newDot])
+
+    console.log(list)
+  }
+
+  const HandleUndo = event => {
+    event.stopPropagation()
+
+    if (list.length === 0) {
+      return
+    }
+
+    const LastDot = list[list.length - 1]
+    setListRedo(prev => [...prev, LastDot])
+
+    setList(prev => [...prev].slice(0, -1))
+  }
+
+  const HandleRedo = event => {
+    event.stopPropagation()
+
+    if (listRedo.length === 0) {
+      return
+    }
+
+    const LastDotRedo = listRedo[listRedo.length - 1]
+    setList(prev => [...prev, LastDotRedo])
+
+    setListRedo(prev => [...prev].slice(0, -1))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app" onClick={Dot}>
+      <button className="button" onClick={HandleUndo}>
+        Remove Dot
+      </button>
+      <button className="button" onClick={HandleRedo}>
+        Return Dot
+      </button>
+      {list.map((item, index) => (
+        <div
+          key={index}
+          className="dot"
+          style={{ top: item.clientY - 5, left: item.clientX - 5 }}
+        ></div>
+      ))}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
